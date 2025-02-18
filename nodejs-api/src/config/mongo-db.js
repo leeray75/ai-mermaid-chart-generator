b001f4a8935714343d41592190fab8b6aed304ae
Raymond Lee <>
2025-02-17 23:07:11 -0500

    issue-30: implement chat history storage

--- nodejs-api/src/config/mongo-db.js
+++ nodejs-api/src/config/mongo-db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const MONGO_URI = process.env.MONGO_URI
   .replace("${MONGO_INITDB_ROOT_USERNAME}", process.env.MONGO_INITDB_ROOT_USERNAME)
   .replace("${MONGO_INITDB_ROOT_PASSWORD}", process.env.MONGO_INITDB_ROOT_PASSWORD);
-  //mongodb://mongoadmin:secret@localhost:27017/
+
 const connectDB = async () => {
   try {
     console.log("[MongoDB] connect URI:", MONGO_URI); // Debugging log
